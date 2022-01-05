9a875896b54dd0507088d60f7edc75083bb4f9fa
Ernesto Tham <ernesto.tham@kbslp.com>
2022-01-04 23:05:45 -0500

    added readme questions

--- index.js
+++ index.js
@@ -6,23 +6,88 @@ console.log("\n\n\n!!!Welcome to the ReadMe Generator!!!\n\n\n")
 inquirer
   .prompt([
     {
-      type: 'input',
-      message: 'What is your user name?',
-      name: 'username',
+      type: 'list',
+      message: 'Menu options',
+      name:'Menu',
+      choices:[
+        new inquirer.Separator(' ============================== '),
+        {
+          name: 'Create ReadMe file',
+        },
+        {
+            name: 'Quit',
+        },
+
+      ],
     },
-    {
-      type: 'password',
-      message: 'What is your password?',
-      name: 'password',
-    },
-    {
-      type: 'password',
-      message: 'Re-enter password to confirm:',
-      name: 'confirm',
-    },
+    
   ])
-  .then((response) =>
-    response.confirm === response.password
-      ? console.log('Success!')
-      : console.log('You forgot your password already?!')
+  .then((response) => {
+   console.log(response['Menu'])
+    if (response['Menu'] === 'Quit'){
+
+    }
+
+    else {
+
+        inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'Description',
+      message: 'Enter ReadMe file Description',
+    },
+    {
+        type: 'input',
+        name: 'Intallation',
+        message: 'Enter installation steps',
+      },
+
+      {
+        type: 'input',
+        name: 'Usage',
+        message: 'Enter usage description',
+      },
+
+      {
+        type: 'input',
+        name: 'Contribuiting',
+        message: 'Enter contribuitors',
+      },
+
+      {
+        type: 'input',
+        name: 'Tests',
+        message: 'Enter Tests',
+      },
+
+      {
+        type: 'list',
+        message: 'choose a license for the application',
+        name: 'license',
+        choices: ['MIT', 'GPL', 'APACHE License 2.0', 'BSD'],
+      },
+
+      {
+        type: 'input',
+        name: 'GitHub Username',
+        message: 'Enter your Github username',
+      },
+
+      {
+        type: 'input',
+        name: 'email address',
+        message: 'Enter your Email address',
+      },
+
+     
+
+  ])
+  .then((data) => {
+    console.log(data)
+  });
+
+    }
+
+}
   );
