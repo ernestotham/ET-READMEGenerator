5170ea7417b9d7ce30350015957b9dc35b069fce
Ernesto Tham <ernesto.tham@kbslp.com>
2022-01-07 00:15:52 -0500

    updated template format

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # ET-READMEGenerator
-JSNode ReadMe Generator
-#samplereadme file
+
+## **ReadMe file generator**
+
+##Description: 
+This application will generate a readme file base on hte input provided
+
+
+##Usage: 
+run index.js in js node
+
+
+##Installation:
+The application will require js node to be installed
+
+
+##Contributing 
+
+
+
+##Questions: 
+Github profile: ernestotha
+
+For any questions reach out to me at ernestotha@gmail.com
+
+
+
+## Table of Contents
+
+
+[Title](#Title)
+
+[Description](#Description)
+
+[Installation](#Installation)
+
+[Usage](#Usage)
+
+[Questions](#Questions)
--- index.js
+++ index.js
@@ -92,44 +92,85 @@ inquirer
   .then((data) => {
     console.log(data)
     const filename ='README.md';
-    console.log(data["Description"])
+    setTimeout(() => {
+        fs.writeFile(`./Output/${filename}`, `## **${data["Title"]}**\n\n`, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });},2000)
+        setTimeout(() => {
+        fs.appendFile(`./Output/${filename}`, `\n\n##Description: \n${data["Description"]}\n`, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });},2000)
+        setTimeout(() => {
+        fs.appendFile(`./Output/${filename}`, `\n\n##Installation:\n${data["Installation"]}\n`, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });},2000)
+        setTimeout(() => {
+        fs.appendFile(`./Output/${filename}`, `\n\n##Usage: \n${data["Usage"]}\n`, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });},2000)
+        setTimeout(() => {
+        fs.appendFile(`./Output/${filename}`, `\n\n##Contributing \n${data["Contributing"]}\n`, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });},2000)
+        setTimeout(() => {
+        fs.appendFile(`./Output/${filename}`, `\n\n##Questions: \nGithub profile: ${data["GitHub"]}\n`, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });},2000)
+        setTimeout(() => {
+        fs.appendFile(`./Output/${filename}`, `\nFor any questions reach out to me at ${data["email"]}\n\n`, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });},2000)
+        setTimeout(() => {
+        fs.appendFile(`./Output/${filename}`, `\n\n## Table of Contents\n\n`, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });},2000)
+        setTimeout(() => {
+        fs.appendFile(`./Output/${filename}`, `\n[Title](#Title)\n`, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });},2000)
+        setTimeout(() => {
+        fs.appendFile(`./Output/${filename}`, `\n[Description](#Description)\n`, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });},2000)
+        if(data["Installation"] !== ""){
+            setTimeout(() => {
+                fs.appendFile(`./Output/${filename}`, `\n[Installation](#Installation)\n`, function (err) {
+                    if (err) throw err;
+                    console.log('Saved!');
+                });},2000)
+            }
+        if(data["Usage"] !== ""){
+            setTimeout(() => {
+            fs.appendFile(`./Output/${filename}`, `\n[Usage](#Usage)\n`, function (err) {
+                if (err) throw err;
+                console.log('Saved!');
+            });},2000)
+        }
 
-    fs.writeFile(`./Output/${filename}`, `# **${data["Title"]}**`, function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-      });
+        if(data["Contributing"] !== ""){
+            setTimeout(() => {
+            fs.appendFile(`./Output/${filename}`, `\n[Contributing](#Contributing)\n`, function (err) {
+                if (err) throw err;
+                console.log('Saved!');
+            });},2000)
+        }
+        setTimeout(() => {
+        fs.appendFile(`./Output/${filename}`, `\n[Questions](#Questions)\n`, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });},2000)
 
-      fs.appendFile(`./Output/${filename}`, `\n\n## ${data["Description"]}\n\n`, function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-      });
-
-      fs.appendFile(`./Output/${filename}`, `\n\n## ${data["Installation"]}\n\n`, function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-      });
-
-      fs.appendFile(`./Output/${filename}`, `\n\n## ${data["Usage"]}\n\n`, function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-      });
-
-      fs.appendFile(`./Output/${filename}`, `\n\n## ${data["Contributing"]}\n\n`, function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-      });
-
-      fs.appendFile(`./Output/${filename}`, `\n\n## ${data["Questions"]}\n\n ${data["Github"]}`, function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-      });
-
-      fs.appendFile(`./Output/${filename}`, `\n\n## Table of Contents\n\n`, function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-      });
-    
-    
+          
   });
 
     }
